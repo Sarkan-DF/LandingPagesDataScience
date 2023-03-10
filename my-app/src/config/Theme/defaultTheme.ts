6d49222dccc93d294bcc436564641b3bf10bacc3
Assferj <assisjuniorcam@gmail.com>
2023-03-10 11:28:52 -0300

    inclusão de ítulo, descrição, botão e alinhamentos do layout

--- my-app/src/config/Theme/defaultTheme.ts
+++ my-app/src/config/Theme/defaultTheme.ts
@@ -1,13 +1,14 @@
-import { createTheme } from '@mui/material';
+import { createTheme } from "@mui/material";
 
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#FFF2F4',
-      contrastText: '#474749'
+      main: "#FFF2F4",
+      contrastText: "#474749",
     },
     secondary: {
-      main: '#DAA520',
+      main: "#DAA520",
+      contrastText: "#fff",
     },
   },
 });
